cf09ca5bd1e6f033ab5040e6ae53fb794d1e10fd
locate <integrityms@gmail.com>
2024-01-09 21:42:00 +0900

    코드 정리

--- get_host_itemid.py
+++ get_host_itemid.py
@@ -1,15 +1,7 @@
-
-from zabbix_api import ZabbixAPI
-import math
+from auth import login, logout
 import time
 import sys
 import json
-
-
-f = open("config.json", encoding="UTF-8")
-CONFIG = json.loads(f.read())
-f.close()
-
 
 argv = sys.argv
 if len(argv) < 2:
@@ -26,25 +18,18 @@ def print_and_write(file, text):
     # print(text)
     print(text, file=file)
 
-def api_login(zabbixName):
-    zabbix = ZabbixAPI(server=CONFIG['ZabbixServerInfo'][zabbixName]['URL'])
-    zabbix.login(CONFIG['ZabbixServerInfo'][zabbixName]['user'],CONFIG['ZabbixServerInfo'][zabbixName]['password'])
-    return zabbix
-
-def getHostList(zabbixName,host_list):
-    zabbix = api_login(zabbixName)
-    getHosts = zabbix.host.get({"selectHosts": ["host"], "selectInterfaces": ["ip", "details"], "output": ["host"], "filter": { "host": host_list }})
+def getHostList(session,host_list):
+    getHosts = session.host.get({"selectHosts": ["host"], "selectInterfaces": ["ip", "details"], "output": ["host"], "filter": { "host": host_list }})
     return getHosts
 
-def getItemList(zabbixName,hostIds):
-    zabbix = api_login(zabbixName)
-    getItems = zabbix.item.get({"output": "extend", "selectHosts": ["name", "hostid","proxy_hostid"], "hostids": hostIds, "filter": { 'type': '7', 'status': '0' }, "sortfield": "name"})
+def getItemList(session,hostIds):
+    getItems = session.item.get({"output": "extend", "selectHosts": ["name", "hostid","proxy_hostid"], "hostids": hostIds, "filter": { 'type': '7', 'status': '0' }, "sortfield": "name"})
     # getItems = zabbix.item.get({"output": "extend", "selectHosts": "extend", "hostids": hostIds, "filter": { 'type': '7', 'status': '1' }, "sortfield": "name"})
     return getItems
 
-
-
 host_list = []
+session = login(zabbix_name)
+
 file_path = host_name_list
 with open(file_path, 'r') as file:
 
@@ -52,21 +37,21 @@ with open(file_path, 'r') as file:
         host = line.strip('\n')
         host_list.append(host)
 
-getHostList = getHostList(zabbix_name,host_list)
+zabbix_host_list = getHostList(session,host_list)
 
 hostIp = {}
-for line in getHostList:
+for line in zabbix_host_list:
     hostIp[line.get('hostid')] = line.get('interfaces')[0].get('ip')
 
 
 # 리스트를 10개씩 묶기
 chunk_size = 10
-result = [getHostList[i:i + chunk_size] for i in range(0, len(getHostList), chunk_size)]
+result = [zabbix_host_list[i:i + chunk_size] for i in range(0, len(zabbix_host_list), chunk_size)]
 
 for host_group in result:
 
     getHostIds = [hostid.get('hostid') for hostid in host_group]
-    ItemList = getItemList(zabbix_name,getHostIds)
+    ItemList = getItemList(session,getHostIds)
     for line in ItemList:
         print(f"{line.get('hosts')[0].get('name')}|{line.get('hostid')}|{hostIp[line.get('hostid')]}|{line.get('itemid')}|{line.get('name')}|{line.get('status')}|{line.get('value_type')}|{line.get('hosts')[0].get('proxy_hostid')}")
     # print(getHostIds)
