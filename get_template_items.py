8afe7383b3a48105b183bdaf482922b77e42a3e7
locate <integrityms@gmail.com>
2024-01-11 18:41:47 +0900

    중간 작업 저장

--- get_template_items.py
+++ get_template_items.py
@@ -31,12 +31,11 @@ def getTemplateList(sesison,templateName,outputFile):
 def getApplicationList(sesison):
     application_datas = sesison.application.get({ "output": ["applicationid", "name"]})
     result = {}
-    result = {}
     for data in application_datas:
         result[data.get('applicationid')] = data.get('name')
 
     return result
-def getItemList(session,template_id_to_name,application_id_to_name):
+def getItemList(session,template_id_to_name):
     templateids = list(template_id_to_name.keys())
     # item_datas = session.item.get({"output": ["itemids", "hostid", "name",  "key_", "status"], "templateids":templateids })
     #{'itemid': '26932', 'type': '12', 'snmp_oid': '', 'hostid': '10171', 'name': 'BB +1.8V SM', 'key_': 'bb_1.8v_sm', 'delay': '1m', 'history': '1w', 'trends': '365d', 'status': '0', 'value_type': '0', 'trapper_hosts': '', 'units': 'V', 'formula': '', 'logtimefmt': '', 'templateid': '0', 'valuemapid': '0', 'params': '', 'ipmi_sensor': 'BB +1.8V SM', 'authtype': '0', 'username': '', 'password': '', 'publickey': '', 'privatekey': '', 'flags': '0', 'interfaceid': '0', 'description': '', 'inventory_link': '0', 'lifetime': '30d', 'evaltype': '0', 'jmx_endpoint': '', 'master_itemid': '0', 'timeout': '3s', 'url': '', 'query_fields': [], 'posts': '', 'status_codes': '200', 'follow_redirects': '1', 'post_type': '0', 'http_proxy': '', 'headers': [], 'retrieve_mode': '0', 'request_method': '0', 'output_format': '0', 'ssl_cert_file': '', 'ssl_key_file': '', 'ssl_key_password': '', 'verify_peer': '0', 'verify_host': '0', 'allow_traps': '0', 'state': '0', 'error': '', 'applications': [{'applicationid': '743'}], 'triggers': [{'triggerid': '17117'}, {'triggerid': '17118'}], 'lastclock': '0', 'lastns': '0', 'lastvalue': '0', 'prevvalue': '0'}
@@ -53,12 +52,20 @@ def getItemList(session,template_id_to_name,application_id_to_name):
                 status = '비활성'
             newItem = []
             newItem.append(template_id_to_name[item.get('hostid')]) # 
-            newItem.append(application_id_to_name[item.get('applications')[0].get('applicationid')])
+            if (len(item.get('applications')) < 1 ):
+                newItem.append('')
+            else:
+                newItem.append(item.get('applications')[0].get('applicationid'))
+                # newItem.append(application_id_to_name[item.get('applications')[0].get('applicationid')])
             #newItem.append(item.get('applications')[0].get('applicationid'))
             newItem.append(item.get('name'))
             newItem.append(item.get('key_'))
             newItem.append(item.get('delay'))
             newItem.append(item.get('history'))
+            if (len(item.get('triggers')) < 1 ):
+                newItem.append('X')
+            else:
+                newItem.append('O')
             newItem.append(item.get('triggers'))
             newItem.append(status)
             newItem_list.append(newItem)
@@ -67,6 +74,19 @@ def getItemList(session,template_id_to_name,application_id_to_name):
         exit()
 
     return newItem_list
+
+def getTriggerList(session,template_id_to_name):
+    templateids = list(template_id_to_name.keys())
+    # item_datas = session.item.get({"output": ["itemids", "hostid", "name",  "key_", "status"], "templateids":templateids })
+    #{'itemid': '26932', 'type': '12', 'snmp_oid': '', 'hostid': '10171', 'name': 'BB +1.8V SM', 'key_': 'bb_1.8v_sm', 'delay': '1m', 'history': '1w', 'trends': '365d', 'status': '0', 'value_type': '0', 'trapper_hosts': '', 'units': 'V', 'formula': '', 'logtimefmt': '', 'templateid': '0', 'valuemapid': '0', 'params': '', 'ipmi_sensor': 'BB +1.8V SM', 'authtype': '0', 'username': '', 'password': '', 'publickey': '', 'privatekey': '', 'flags': '0', 'interfaceid': '0', 'description': '', 'inventory_link': '0', 'lifetime': '30d', 'evaltype': '0', 'jmx_endpoint': '', 'master_itemid': '0', 'timeout': '3s', 'url': '', 'query_fields': [], 'posts': '', 'status_codes': '200', 'follow_redirects': '1', 'post_type': '0', 'http_proxy': '', 'headers': [], 'retrieve_mode': '0', 'request_method': '0', 'output_format': '0', 'ssl_cert_file': '', 'ssl_key_file': '', 'ssl_key_password': '', 'verify_peer': '0', 'verify_host': '0', 'allow_traps': '0', 'state': '0', 'error': '', 'applications': [{'applicationid': '743'}], 'triggers': [{'triggerid': '17117'}, {'triggerid': '17118'}], 'lastclock': '0', 'lastns': '0', 'lastvalue': '0', 'prevvalue': '0'}
+    #triggers': [{'triggerid': '17117', 'expression': '{20124}<1.597 or {20124}>2.019', 'description': 'BB +1.8V SM Critical [{ITEM.VALUE}]', 'url': '', 'status': '0', 'value': '0', 'priority': '5', 'lastchange': '0', 'comments': '', 'error': '', 'templateid': '0', 'type': '0', 'state': '0', 'flags': '0', 'recovery_mode': '0', 'recovery_expression': '', 'correlation_mode': '0', 'correlation_tag': '', 'manual_close': '0', 'opdata': ''
+    #item_datas = session.item.get({ "selectTriggers": ["expression","description","status","recovery_expression","priority" "selectApplications","opdata"], "expandExpression": "y", "selectApplications": "true", "output": ["hostid","name","key_","delay","history"], "templateids":templateids })
+    trigger_datas = session.trigger.get({ "selectTriggers": ["triggerid"],  "expandExpression": "y", "expandDescription": "y", "output": ["expression","description","status","recovery_expression","priority" "selectApplications","opdata"], "templateids":templateids })
+    result = {}
+    for data in trigger_datas:
+        result[data.get('hostid')] = data.get('name')
+    return trigger_datas
+
 # 파일리스트 가져오기
 file_path = template_list_file
 with open(file_path, 'r') as file:
@@ -74,16 +94,21 @@ with open(file_path, 'r') as file:
     template_list = [line for line in lines]
 
 session = login(zabbix_name)
+# application_id_to_name = getApplicationList(session)
 template_id_to_name = getTemplateList(session,template_list,'')
-application_id_to_name = getApplicationList(session)
-item_list = getItemList(session,template_id_to_name,application_id_to_name)
-for item in item_list:
-     if (len(item[6]) == 0):
-        item[6] = ""
-     else:
-         item[6] = ", ".join([sub_dict['triggerid'] for sub_dict in item[6]])
+item_list = getItemList(session,template_id_to_name)
+trigger_list = getTriggerList(session,template_id_to_name)
+for trigger in trigger_list:
+    print(trigger)
 
-     print('|'.join(map(str, item)))
+exit()
+for item in item_list:
+     if (len(item[7]) == 0):
+        item[7] = ""
+     else:
+         item[7] = ", ".join([sub_dict['triggerid'] for sub_dict in item[7]])
+
+     print('!'.join(map(str, item)))
     # if (len(item[6]) > 0):
     #     for trigger in item[6]:
     #         print(trigger)     
