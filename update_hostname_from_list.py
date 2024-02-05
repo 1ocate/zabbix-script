6671818041a04a47424dff67348e8f0f974d9367
locate <integrityms@gmail.com>
2024-02-05 23:24:59 +0900

    앞으로 추가할 코드의 주석 추가

--- update_hostname_from_list.py
+++ update_hostname_from_list.py
@@ -55,6 +55,8 @@ if len(exsist_host_list) > 0 :
         form = {}
         form['hostid'] = line.get("hostid")
         exsist_hostids.append(form)
+    # delete group "duplicate_hosts"
+    # create group "duplicate_hosts"
 
     messUpdateHostId(session,"duplicate_hosts",exsist_hostids,zabbix_name)
     print("변경 될 호스트 이름의 호스트가 존재 합니다. 삭제하고 진행하세요.")
