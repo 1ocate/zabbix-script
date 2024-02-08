258d3690a9158bec6e0a4c33cf919fff6b1832e0
locate <integrityms@gmail.com>
2024-02-08 23:42:00 +0900

    코드 정리

--- get_host.py
+++ get_host.py
@@ -1,13 +1,10 @@
 
 from zabbix_api import ZabbixAPI
+from auth import login, logout
 import math
 import time
 import sys
 import json
-
-f = open("config.json", encoding="UTF-8")
-CONFIG = json.loads(f.read())
-f.close()
 
 
 argv = sys.argv
@@ -25,32 +22,27 @@ def print_and_write(file, text):
     # print(text)
     print(text, file=file)
 
-def api_login(zabbixName):
-    zabbix = ZabbixAPI(server=CONFIG['ZabbixServerInfo'][zabbixName]['URL'])
-    zabbix.login(CONFIG['ZabbixServerInfo'][zabbixName]['user'],CONFIG['ZabbixServerInfo'][zabbixName]['password'])
-    return zabbix
 
-def getHostList(zabbixName,host_list):
-    zabbix = api_login(zabbixName)
-    params = {"selectHosts": ["host"], "selectInterfaces": ["ip", "details"], "output": ["host"], "filter": { "ip": host_list }}
-    getHosts = zabbix.host.get(params)
-
+def getHostList(session,zabbix_name,host_list):
+    getHosts = session.host.get({"selectHosts": ["host"], "selectInterfaces": ["ip", "details"], "output": ["host"], "filter": { "host": host_list }})
+    if getHosts == []:
+        getHosts = session.host.get({"selectHosts": ["host"], "selectInterfaces": ["ip", "details"], "output": ["host"], "filter": { "ip": host_list }})
     return getHosts
 
-
-host_list = []
+search_list = []
 file_path = host_name_list
 with open(file_path, 'r') as file:
 
     for line in file:
         host = line.strip('\n')
-        host_list.append(host)
+        search_list.append(host)
 
-getHostList = getHostList(zabbix_name,host_list)
+session = login(zabbix_name)
+host_list = getHostList(session,zabbix_name,search_list)
 
-for line in getHostList:
+for line in host_list:
     print(f"{line.get('host')}|{line.get('interfaces')[0].get('ip')}")
     # print(f"{line.get('interfaces')[0].get('ip')}")
 
-print(f"총 검색 대상 갯수 {len(host_list)}")
-print(f"총 검색 결과 갯수 {len(getHostList)}")
+print(f"총 검색 대상 갯수 {len(search_list)}")
+print(f"총 검색 결과 갯수 {len(host_list)}")
