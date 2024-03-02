1debafcfcaf4a4ae687b7eb76822025f7940ed81
locate <integrityms@gmail.com>
2024-03-02 21:15:25 +0900

    찾는 템플릿이 없는 경우 출력 수정

--- get_template_from_list.py
+++ get_template_from_list.py
@@ -88,51 +88,47 @@ def getItemList(session,template_id_to_name,discovery):
     else:
         item_datas = session.item.get({ "selectTriggers": ["triggerid"], "expandExpression": "y", "selectApplications": ["name"], "output": ["templateid", "hostid", "name", "key_", "delay", "history", "status"], "templateids":templateids })
     newItem_list= []
-    if len(item_datas) != 0:
-        for item in item_datas:
-            status = ''
-            if item.get('status') == '0':
-                status = '활성'
-            elif item.get('status') == '1':
-                status = '비활성'
-            newItem = []
-            newItem.append(template_id_to_name[item.get('hostid')]) # 
-            if (len(item.get('applications')) < 1 ):
-                newItem.append('None')
-            else:
-                newItem.append(item.get('applications')[0].get('name'))
-            template_id = (item.get('templateid'))
-            item_name = item.get('name')
-            # if template_id != "0":
-            #     item_name = f"{template_id_to_name[item.get('templateid')]}: {item_name}"
+    for item in item_datas:
+        status = ''
+        if item.get('status') == '0':
+            status = '활성'
+        elif item.get('status') == '1':
+            status = '비활성'
+        newItem = []
+        newItem.append(template_id_to_name[item.get('hostid')]) # 
+        if (len(item.get('applications')) < 1 ):
+            newItem.append('None')
+        else:
+            newItem.append(item.get('applications')[0].get('name'))
+        template_id = (item.get('templateid'))
+        item_name = item.get('name')
+        # if template_id != "0":
+        #     item_name = f"{template_id_to_name[item.get('templateid')]}: {item_name}"
 
-            newItem.append(item_name)
+        newItem.append(item_name)
 
-            # discovery값이 'y'면 prototype
-            if discovery == 'y':
-                newItem.append("O")
-            else:
-                newItem.append("X")
+        # discovery값이 'y'면 prototype
+        if discovery == 'y':
+            newItem.append("O")
+        else:
+            newItem.append("X")
 
-            newItem.append(item.get('key_'))
-            newItem.append(item.get('delay'))
-            newItem.append(item.get('history'))
-            if (len(item.get('triggers')) < 1 ):
-                newItem.append(status)
-                newItem.append('X')
-                newItem.append('')
-                newItem.append('')
-                newItem.append('')
-                newItem.append('')
-                newItem.append('')
-            else:
-                newItem.append(status)
-                newItem.append('O')
-                newItem.append(item.get('triggers'))
-            newItem_list.append(newItem)
-    else:
-        print("아이템을 찾을 수 없습니다.")
-
+        newItem.append(item.get('key_'))
+        newItem.append(item.get('delay'))
+        newItem.append(item.get('history'))
+        if (len(item.get('triggers')) < 1 ):
+            newItem.append(status)
+            newItem.append('X')
+            newItem.append('')
+            newItem.append('')
+            newItem.append('')
+            newItem.append('')
+            newItem.append('')
+        else:
+            newItem.append(status)
+            newItem.append('O')
+            newItem.append(item.get('triggers'))
+        newItem_list.append(newItem)
     return newItem_list
 
 def getTriggerList(session,template_id_to_name,discovery):
@@ -193,13 +189,16 @@ with open(file_path, 'r') as file:
     else: 
         template_list = lines
 session = login(zabbix_name)
-# application_id_to_name = getApplicationList(session,)
 template_id_to_name = getTemplateList(session,template_list,'')
 trigger_id_to_data = getTriggerList(session,template_id_to_name,'')
 triggerprototype_id_to_data = getTriggerList(session,template_id_to_name,'y')
 item_list = getItemList(session,template_id_to_name,'')
 itemprototype_list = getItemList(session,template_id_to_name,'y')
 discovery_list = getDiscoveryList(session,template_id_to_name)
+if len(item_list) == 0 and len(itemprototype_list) == 0 and len(discovery_list) == 0:
+    print("아이템을 찾을 수 없습니다.")
+    sys.exit()
+
 # 결과 출력 
 process_item_list(item_list, trigger_id_to_data)
 process_item_list(itemprototype_list, triggerprototype_id_to_data)
