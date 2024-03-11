bd6056085020342d9b499022a7c49b3a6120d1ea
locate <integrityms@gmail.com>
2024-03-11 21:39:58 +0900

    auth 함수 사용하여 로그인, 로그아웃

--- get_group_host.py
+++ get_group_host.py
@@ -1,11 +1,6 @@
-
 from zabbix_api import ZabbixAPI
+from auth import login, logout
 import sys
-import json
-
-f = open("config.json", encoding="UTF-8")
-CONFIG = json.loads(f.read())
-f.close()
 
 argv = sys.argv
 if len(argv) < 2:
@@ -15,23 +10,19 @@ else:
     zabbix_name = argv[1]
     group_name = argv[2]
 
+session = login(zabbix_name)
+
 def print_and_write(file, text):
     # print(text)
     print(text, file=file)
 
-def api_login(zabbixName):
-    zabbix = ZabbixAPI(server=CONFIG['ZabbixServerInfo'][zabbixName]['URL'])
-    zabbix.login(CONFIG['ZabbixServerInfo'][zabbixName]['user'],CONFIG['ZabbixServerInfo'][zabbixName]['password'])
-    return zabbix
-
-def getHostList(zabbixName,groupName):
-    zabbix = api_login(zabbixName)
-    getGroup = zabbix.hostgroup.get({ "output": ["groupids"],  "filter": { "name": groupName }})
+def getHostList(session,groupName):
+    getGroup = session.hostgroup.get({ "output": ["groupids"],  "filter": { "name": groupName }})
     if getGroup == []:
         print("그룹을 찾을 수 없습니다.")
         exit()
     getGroupid = getGroup[0].get('groupid')
-    getHosts = zabbix.host.get({"selectHosts": ["host"], "selectInterfaces": ["ip", "details"], "output": ["host"], "groupids":getGroupid })
+    getHosts = session.host.get({"selectHosts": ["host"], "selectInterfaces": ["ip", "details"], "output": ["host"], "groupids":getGroupid })
     return getHosts
 
 result = getHostList(zabbix_name,group_name)
@@ -39,3 +30,5 @@ result = getHostList(zabbix_name,group_name)
 for line in result:
     #print(f"{line.get('host')}")
     print(f"{line.get('host')}|{line.get('interfaces')[0].get('ip')}")
+
+logout(session)
